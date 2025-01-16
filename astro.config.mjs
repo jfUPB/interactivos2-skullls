e67cf842feca0c4d5b45e9740453c9f23cec558f
Juan Franco WSL <juanf.franco@upb.edu.co>
2025-01-16 14:16:59 -0500

    Add support to math

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import remarkMath from 'remark-math';
+import rehypeMathjax from 'rehype-mathjax';
 
 // https://astro.build/config
 export default defineConfig({
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeMathjax],
+	},
 	integrations: [
 		starlight({
 			title: 'Sistemas Físicos Interactivos 2',
